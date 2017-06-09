4aec64194bc86c53fee18c255ea5c9080cd5f652
Nate Genter <ngenter@plasticingenuity.local>
2017-06-09 15:28:37 -0500

    Created Calculator Class, and moved calculation methods

---
+++ Calculator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpConsoleCalculator;
+
+namespace CsharpConsoleCalculator
+{
+    class Calculator
+    {
+        public static void Addition()
+        {
+            var sum = 0;
+            Console.WriteLine("\nWelcome to the addition section\n");
+            Console.WriteLine("Enter numbers seperated by a comma and I will give you the sum\n");
+            var input = Console.ReadLine();
+
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+
+            foreach (var number in input.Split(','))
+            {
+                var convertedNumber = Convert.ToInt32(number);
+                sum = sum += convertedNumber;
+
+
+            }
+
+            foreach (var number in input.Split(','))
+            {
+                Console.Write("{0} + ", number);
+            }
+            Console.WriteLine("\n{0}\n", sum);
+
+
+            Program.Menu();
+        }
+
+        public static void Subtraction()
+        {
+            Console.WriteLine("\nWelcome to the subtraction section\n");
+            Console.WriteLine("Enter the number you would like to subtract from");
+            var firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the amount you would like subtract");
+            var secondNumber = Convert.ToInt32(Console.ReadLine());
+            var total = (firstNumber - secondNumber);
+
+            Console.WriteLine("\n{0} - {1} = {2} \n", firstNumber, secondNumber, total);
+
+            Program.Menu();
+
+        }
+
+        public static void Multiplication()
+        {
+            List<int> intList = new List<int>();
+            var num = 1;
+            Console.WriteLine("\nWelcome to the multiplication section\n");
+            Console.WriteLine("Enter the first number you want to multiply");
+            var input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second number you want to multiply");
+            var input2 = Convert.ToInt32(Console.ReadLine());
+            var total = (input * input2);
+            Console.WriteLine("{0} X {1} = {2}\n", input, input2, total);
+
+            Program.Menu();
+        }
+
+        public static void Division()
+        {
+            Console.WriteLine("\nWelcome to the division section\n");
+            Console.WriteLine("Enter the numerator");
+            var numerator = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the denominator");
+            var denominator = Convert.ToInt32(Console.ReadLine());
+            var product = (numerator / denominator);
+            Console.WriteLine("\n{0} / {1} = {2}\n", numerator, denominator, product);
+
+            Program.Menu();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using CsharpConsoleCalculator;
 
 namespace CSharpConsoleCalculator
 {
@@ -27,16 +28,16 @@ namespace CSharpConsoleCalculator
             switch (n)
             {
                 case 1:
-                    Addition();
+                    Calculator.Addition();
                     break;
                 case 2:
-                    Subtraction();
+                    Calculator.Subtraction();
                     break;
                 case 3:
-                    Multiplication();
+                    Calculator.Multiplication();
                     break;
                 case 4:
-                    Division();
+                    Calculator.Division();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid selection\n");
@@ -45,79 +46,13 @@ namespace CSharpConsoleCalculator
             }
         }
 
-        public static void Addition()
-        {
-            var sum = 0;
-            Console.WriteLine("\nWelcome to the addition section\n");
-            Console.WriteLine("Enter numbers seperated by a comma and I will give you the sum\n");
-            var input = Console.ReadLine();
+        
 
-            if (String.IsNullOrWhiteSpace(input))
-            {
-                Console.WriteLine("Error");
-                return;
-            }
+        
 
-            foreach (var number in input.Split(','))
-            {
-                var convertedNumber = Convert.ToInt32(number);
-                sum = sum += convertedNumber;
+        
 
-
-            }
-
-            foreach (var number in input.Split(','))
-            {
-                Console.Write("{0} + ", number);
-            }
-            Console.WriteLine("\n{0}\n", sum);
-
-
-            Menu();
-        }
-
-        public static void Subtraction()
-        {
-            Console.WriteLine("\nWelcome to the subtraction section\n");
-            Console.WriteLine("Enter the number you would like to subtract from");
-            var firstNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the amount you would like subtract");
-            var secondNumber = Convert.ToInt32(Console.ReadLine());
-            var total = (firstNumber - secondNumber);
-
-            Console.WriteLine("\n{0} - {1} = {2} \n", firstNumber, secondNumber, total);
-
-            Menu();
-
-        }
-
-        public static void Multiplication()
-        {
-            List<int> intList = new List<int>();
-            var num = 1;
-            Console.WriteLine("\nWelcome to the multiplication section\n");
-            Console.WriteLine("Enter the first number you want to multiply");
-            var input = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the second number you want to multiply");
-            var input2 = Convert.ToInt32(Console.ReadLine());
-            var total = (input * input2);
-            Console.WriteLine("{0} X {1} = {2}\n", input, input2, total);
-
-            Menu();
-        }
-
-        public static void Division()
-        {
-            Console.WriteLine("\nWelcome to the division section\n");
-            Console.WriteLine("Enter the numerator");
-            var numerator = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the denominator");
-            var denominator = Convert.ToInt32(Console.ReadLine());
-            var product = (numerator / denominator);
-            Console.WriteLine("\n{0} / {1} = {2}\n", numerator, denominator, product);
-
-            Menu();
-        }
+        
         public static void Intro()
         {
             Console.WriteLine("Welcome to the calculator program, enter the number that corresponds with the section you want to use\n");
